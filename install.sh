e535146856445f7fd6b44f15573400e71db41f1a
sakthivelCardinality <sakthivel.a@cardinality.ai>
2023-06-22 00:38:06 +0530

    :tada: :sparkles: initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/config/.user_alias ~/.user_alias
+ln -sf ~/dotfiles/config/.user_config ~/.user_config
