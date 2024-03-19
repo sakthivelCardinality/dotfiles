8a6f86efb3eb85b7f44107627cef6c68611e096c
sakthivelCardinality <sakthivel.a@cardinality.ai>
2024-03-19 17:50:36 +0530

    :tada: :sparkles: added the cronjob config

--- config/gitconfig.sh
+++ config/gitconfig.sh
@@ -9,10 +9,14 @@ git config --global cloumn.ui auto
 echo "git config --global branch.sort committerdate"
 git config --global branch.sort committerdate
 
+echo "squash-all alias created for the git"
+# git squash-all -m "a brand new start"
+git config --global alias.squash-all '!f(){ git reset $(git commit-tree HEAD^{tree} "$@");};f'
+
 # echo "git config --global core.fsmonitor true"
 # git config --global core.fsmonitor true
 
 # Run the below command in the git project.
-# this will run cronjob in the project dir 
+# this will run cronjob in the project dir
 # which will run garbage collection in the background which will run at the time of git commint
-# git maintenance start 
+# git maintenance start
